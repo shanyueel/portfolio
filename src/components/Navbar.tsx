88743d3889de5f496cc126b1310fdfa41a934506
shanyueel <wulingkevin0704@gmail.com>
2024-05-09 01:06:01 +0800

    feat: routes and Navbar settings

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <header className="header">
+      <NavLink to="/" className="flex items-center justify-center w-10 h-10 rounded-l bg-white font-bold shadow-md ">
+        <p className="blue-gradient_text">AH</p>
+      </NavLink>
+      <nav className="flex gap-7 text-lg font-medium">
+        <NavLink to="/about" className={({ isActive }) => (isActive ? 'text-blue-500' : 'text-black')}>
+          About
+        </NavLink>
+        <NavLink to="/projects" className={({ isActive }) => (isActive ? 'text-blue-500' : 'text-black')}>
+          Projects
+        </NavLink>
+      </nav>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
